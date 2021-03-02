b99d2bf34969dffd688471d9c1b71d6a1532cb2d
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-03-02 18:40:18 -0500

    set up nextjs project for quiz app

---
+++ pages/api/jservice.js
@@ -0,0 +1,28 @@
+const handler = (req, res) => {
+  return res.json({ hello: 'world!' });
+};
+export default handler;
+
+
+
+
+
+
+
+// random question endpoint: http://jservice.io/api/random
+
+// var request = new XMLHttpRequest()
+
+// request.open('GET', 'http://jservice.io/api/random', true)
+//   var data = JSON.parse(this.response)
+
+//   if (request.status >= 200 && request.status < 400) {
+
+
+//   }
+
+// request.onload = function () {
+
+// }
+
+// request.send();
